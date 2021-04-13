90b2aad57162559ffca517eaa1d000099235be58
LindseyBowen <lindseyjbowen@gmail.com>
2021-04-13 19:58:34 -0400

    deploying'

---
+++ client/build/precache-manifest.9d0b5d6735ab06c15ae44ce49e9d047e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "414ebccc02df4cdfe124",
+    "url": "/static/css/main.4c58eb8f.chunk.css"
+  },
+  {
+    "revision": "414ebccc02df4cdfe124",
+    "url": "/static/js/main.3ad0d7c0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a0ba9c59e9702c357adc",
+    "url": "/static/js/2.b07f66f0.chunk.js"
+  },
+  {
+    "revision": "ac0d5ac83ec03439a9400ebf6c9e0465",
+    "url": "/static/media/Books3.ac0d5ac8.jpg"
+  },
+  {
+    "revision": "25ebfa25754f4e8c2b4da3a5976c26b6",
+    "url": "/index.html"
+  }
+];
--- server.js
+++ server.js
@@ -5,10 +5,13 @@ const port = process.env.PORT || 3001;
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.use(require("./routes/api-routes"))
-
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+  }
+  
 const mongoose=require("mongoose")
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks",
+mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://lindseybowen:<Raffles@2002>@cluster0.ise6s.mongodb.net/googlebooks?retryWrites=true&w=majority",
 {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -21,7 +24,5 @@ app.listen(port,function(){
     console.log("app is listening on port"+port)
 })
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-  }
+
   
